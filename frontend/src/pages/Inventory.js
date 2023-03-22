6d457d9e4e642e092cdce983fb89b59e192e91cc
srinivas1591 <devisrinivas.vasamsetti@gmail.com>
2023-03-22 21:32:43 +0530

    Ui Changes for invemtory

--- frontend/src/pages/Inventory.js
+++ frontend/src/pages/Inventory.js
@@ -1,28 +1,81 @@
-import React from 'react'
-import { FormControl, InputLabel, Input} from '@mui/material';
+import React, {useState} from 'react'
 import '../assets/css/custom.css'
+import { Button, Modal, Input } from 'antd';
 
 const Inventory = () => {
 
-const onImageChange = () => {
+ const [isModalOpen, setIsModalOpen] = useState(false);
+ const [title, setTitle] = useState('');
+ const [okText, setOkText] = useState('');
+ const [showImageField, setShowImageField] = useState(false);
+ const [editFunction, setEditFunction] = useState();
 
-}
+ const showModal = () => {
+    setTitle('Add Item');
+    setOkText('Add');
+    setShowImageField(true);
+    setIsModalOpen(true);
+ }
+
+ const handleCancel = () => {
+    defaultStateVariables();
+    setIsModalOpen(false);
+ }
+
+ const handleAdd = () => {
+    setIsModalOpen(false);
+ }
+
+ const handleUpdate = () => {
+    setIsModalOpen(false);
+ }
+
+ const defaultStateVariables = () => {
+    setTitle('');
+    setOkText('');
+ }
+
+ const showEditModal = () => {
+    setTitle('Edit Item');
+    setOkText('Update');
+    setShowImageField(false);
+    setIsModalOpen(true);
+ }
 
   return (
     <div>
-        <h1>Add Items</h1>
-        <div className='flexBox'>
-            <FormControl className='formField'>
-                <InputLabel htmlFor="my-input">Item Name</InputLabel>
-                <Input id="my-input" aria-describedby="my-helper-text" />
-            </FormControl>
-            <FormControl className='formField'>
-                <InputLabel htmlFor="my-input">Quantity</InputLabel>
-                <Input id="my-input" aria-describedby="my-helper-text" />
-            </FormControl>
+
+        <div style={{width: 'auto', height: '20px'}}></div>
+
+        <div className='stickyHeader'>
+            <span className='spanText'>Add Item to Inventory</span>
             <div>
-                <input type="file" onChange={onImageChange} className="filetype" />
+            <Button type="primary" onClick={showModal}>
+                Add Item
+            </Button>
+            <Modal title={title} open={isModalOpen} onOk={editFunction} onCancel={handleCancel} okText={okText}>
+                <Input className='modalStyles' placeholder="Name" />
+                <Input className='modalStyles' placeholder="Quantity"  type="number"/>
+                {showImageField && <Input className='modalStyles' placeholder="Image"  type="file" accept="image/png, image/gif, image/jpeg"/>}
+            </Modal>
             </div>
+        </div>
+
+        <div style={{width: 'auto', height: '20px'}}></div>
+
+        <div className='flexBox'>
+            <div className='itemBox'>
+                <img src='/img/hera.jpg' style={{ width: "200px", height: '200px' }}></img>
+                <div className='innerFlex'>
+                    <p className='nameText'>Name</p>
+                    <p className='quantityText'>Quantity</p>
+                    <Button type="primary" onClick={showEditModal}>
+                        Edit Item
+                    </Button>
+                </div>
+            </div>
+
+            <div style={{width: 'auto', height: '20px'}}></div>
         </div>
     </div>
   )
