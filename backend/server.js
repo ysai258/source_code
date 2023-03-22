d417fadb19eb31d9cfbc74a3617eaf701303fa68
srinivas1591 <devisrinivas.vasamsetti@gmail.com>
2023-03-22 19:20:25 +0530

    mongo db integrated

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
-require('dotenv').config();
 import express from "express";
 import mongoose from "mongoose";
 import Cors from "cors";
+import dotenv from 'dotenv';
+import dotenvExpand from 'dotenv-expand'
 
+const myEnv = dotenv.config()
+dotenvExpand.expand(myEnv)
 // App Config
 
 const app = express();
@@ -11,18 +14,23 @@ const port = process.env.port || 8001;
 // midde ware
 app.use(express.json())
 app.use(Cors());
+
 // DB config
+const connectionParams = {
+useNewUrlParser: true,
+useUnifiedTopology: true,
+};
+try {
+    mongoose.connect(
+        `mongodb+srv://gallasathvika2001:${process.env.mongoPassword}@cluster0.row0apk.mongodb.net/${process.env.mongodbName}?retryWrites=true&w=majority`,
+        connectionParams
+    );
+    console.log("Database connected succesfully");
+} catch (error) {
+    console.log(error);
+    console.log("Database connection failed");
+}
 
-//mongodb integration
-const url = `mongodb+srv://gallasathvika2001:${process.env.mongoPassword}@cluster0.row0apk.mongodb.net/${process.env.mongodbName}?retryWrites=true&w=majority`;
-
-mongoose.connect(url);
-
-const connection = mongoose.connection;
-
-connection.once('open', ()=>{
-    console.log('MongoDB connected');
-})
 
 //API Endpoint
 
